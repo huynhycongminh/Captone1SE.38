84ba992b02a8abdb099eb67eff521c967888b345
huynhycongminh <huynhycongminhgl2017@gmail.com>
2021-09-22 10:23:31 +0700

    file chua menu

---
+++ js/style.js
@@ -0,0 +1,9 @@
+$(".autoplay").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+  nextArrow: '<button type="button" class="slick-next"></button>',
+  arrows: true,
+});
