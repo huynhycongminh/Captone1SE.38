897b2fccb3d0db4823ef341143e67fcd7175b227
huynhycongminh <huynhycongminhgl2017@gmail.com>
2021-09-22 10:27:05 +0700

    khong co gi

--- js/style.js
+++
@@ -1,9 +0,0 @@
-$(".autoplay").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  prevArrow: '<button type="button" class="slick-prev"></button>',
-  nextArrow: '<button type="button" class="slick-next"></button>',
-  arrows: true,
-});
