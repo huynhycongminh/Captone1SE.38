8243fe1d7c8941fd5f0900b20af6840ad9f5f806
huynhycongminh <huynhycongminhgl2017@gmail.com>
2021-09-25 15:05:19 +0700

    home page

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// hieu dung slide
+$(".autoplay").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  // prevArrow: '<button type="button" class="slick-prev"></button>',
+  // nextArrow: '<button type="button" class="slick-next"></button>',
+  // arrows: true,
+});
